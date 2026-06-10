dfdc3e6fef5ce6c6cd20354cce900071023fc57e
Koemseang <koemseang.phoeurn@student.passerellesnumeriques.org>
2026-06-10 15:40:57 +0700

    first commit

---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\Api\CategoryController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+// Create Route API
+
+Route::apiResource('/categories', [CategoryController::class, 'index']);
