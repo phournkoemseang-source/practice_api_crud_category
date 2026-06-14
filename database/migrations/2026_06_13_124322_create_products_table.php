9fe38654fa2a54591b22abfd0c7c43ab4fe71be0
phournkoemseang-source <phournkoemseang@gmail.com>
2026-06-14 13:31:54 +0700

    migrate table product into mysql

--- database/migrations/2026_06_13_124322_create_products_table.php
+++ database/migrations/2026_06_13_124322_create_products_table.php
@@ -4,18 +4,26 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+
+            $table->foreignId('category_id')
+                ->constrained('categories')
+                ->onDelete('cascade');
+
+            $table->string('name');
+            $table->string('image')->nullable();
+            $table->decimal('price', 10, 2);
+            $table->integer('stock');
+            $table->boolean('is_active')->default(true);
+
             $table->timestamps();
         });
     }
+
 
     /**
      * Reverse the migrations.
